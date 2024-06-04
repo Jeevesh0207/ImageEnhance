8d7d5e86b199493ad666b6dfbbcd3e1cbc417a9a
Jeevesh0207 <r27abbit@gmail.com>
2024-06-04 14:47:42 +0530

    add cross origin

--- app.py
+++ app.py
@@ -1,37 +1,51 @@
 from flask import Flask, request, jsonify
 from flask import send_file
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import cv2
 import numpy as np
 import io
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+CORS.cross_origin(
+    origins='*',
+    methods=['GET', 'HEAD', 'POST', 'OPTIONS', 'PUT']
+)
+
 
 # Function to adjust brightness, contrast, and color saturation
 def adjust_image_properties(image, alpha, beta, saturation):
-    new_image = cv2.addWeighted(image, alpha, np.zeros(image.shape, image.dtype), 0, beta)
+    new_image = cv2.addWeighted(image, alpha, np.zeros(
+        image.shape, image.dtype), 0, beta)
     hsv_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2HSV)
     hsv_image[:, :, 1] = cv2.add(hsv_image[:, :, 1], saturation)
     final_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
     return final_image
 
 # Function to soften the image using a bilateral filter
+
+
 def soften_image(image):
-    softened_image = cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
+    softened_image = cv2.bilateralFilter(
+        image, d=9, sigmaColor=75, sigmaSpace=75)
     return softened_image
 
 # Function to detect edges and smooth them
+
+
 def smooth_edges(image):
     edges = cv2.Canny(image, 100, 200)
-    dilated_edges = cv2.dilate(edges, np.ones((3,3), np.uint8), iterations=1)
-    smooth_edges = cv2.bilateralFilter(dilated_edges, d=9, sigmaColor=75, sigmaSpace=75)
+    dilated_edges = cv2.dilate(edges, np.ones((3, 3), np.uint8), iterations=1)
+    smooth_edges = cv2.bilateralFilter(
+        dilated_edges, d=9, sigmaColor=75, sigmaSpace=75)
     edge_mask = smooth_edges != 0
     softened_image_with_smooth_edges = soften_image(image)
     softened_image_with_smooth_edges[edge_mask] = image[edge_mask]
     return softened_image_with_smooth_edges
 
+
 @app.route('/enhance-image', methods=['POST'])
+@cross_origin()
 def enhance_image():
     if 'image' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
@@ -56,5 +70,6 @@ def enhance_image():
     # Send the enhanced image back to the client
     return send_file(io_buf, mimetype='image/jpeg')
 
+
 if __name__ == '__main__':
-    app.run(debug=True,port=5001)
+    app.run(debug=True, port=5001)
